bdf859dc5ee266f7195e30886feb23dfefa41c34
novus318 <nizamudheen318@gmail.com>
2024-12-28 22:24:15 +0530

    error

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,19 @@
-import path from "path"
-import react from "@vitejs/plugin-react-swc"
-import { defineConfig } from "vite"
+import path from 'path';
+import react from '@vitejs/plugin-react-swc';
+import { defineConfig } from 'vite';
 
 export default defineConfig({
-  server:{
-    hmr:{
+  server: {
+    hmr: {
       overlay: false,
-    }
+    },
+    host: '0.0.0.0', // Ensure the server is accessible on all interfaces
+    port: 3000, // Use the PORT environment variable or default to 3000
   },
   plugins: [react()],
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "./src"),
+      '@': path.resolve(__dirname, './src'),
     },
   },
-})
+});
